238ab752f2fa4deba3d7018b573f077b70192bfd
AlinurAbdyiskakov <75415137+AlinurAbdyiskakov@users.noreply.github.com>
2023-02-22 17:56:41 +0600

    add new comit

--- page12.js
+++ page12.js
@@ -1,57 +1,60 @@
-let comments = [];
-let btn = document.querySelector('button');
-const lo = () => {
-    alert('Спасибо за отзыв');
-}
-btn.addEventListener('click', lo);
-loadComments();
-document.getElementById('comment-add').onclick = function() {
-    event.preventDefault();
-    let commentName = document.getElementById('comment-name');
-    let commentBody = document.getElementById('comment-body');
-
-    let comment = {
-
-        name: commentName.value,
-        body: commentBody.value,
-        time: Math.floor(Date.now())
-    }
-    commentName.value = '';
-    commentBody.value = '';
-    comments.push(comment);
-    saveComments();
-    showComments();
-    console.log(comment);
-
+var firebaseConfig = {
+    apiKey: "AIzaSyB6CMwkqV77uOMOswg7JaLWkMGjLOA3LX4",
+    authDomain: "kafe-51912.firebaseapp.com",
+    databaseURL: "https://kafe-51912-default-rtdb.firebaseio.com",
+    projectId: "kafe-51912",
+    storageBucket: "kafe-51912.appspot.com",
+    messagingSenderId: "487624418851",
+    appId: "1:487624418851:web:a1d9b5cff3fed92bf63396",
+    measurementId: "G-R39J9MYEPZ"
 };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+// initialize database
+const db = firebase.database();
 
+// get user's data
+const username = prompt("Ваша имя и спасибо за отзыв");
 
-function saveComments() {
-    localStorage.setItem('comments', JSON.stringify(comments));
+// submit form
+// listen for submit event on the form and call the postChat function
+document.getElementById("message-form").addEventListener("submit", sendMessage);
 
-}
+// send message to db
+function sendMessage(e) {
+    e.preventDefault();
 
-function loadComments() {
-    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
-    showComments();
-}
+    // get values to be submitted
+    const timestamp = Date.now();
+    const messageInput = document.getElementById("message-input");
+    const message = messageInput.value;
 
-function showComments() {
-    let commentfiled = document.getElementById('comment-field');
-    let out = '';
-    comments.forEach(function(item) {
-        out += `<p class="text-right small"><em>${timeConverter(item.time)}</em></p>`
-        out += `<p class="alert alert-primary">${item.name}</p>`
-        out += `<p class="alert alert-success">${item.body}</p>`
+    // clear the input box
+    messageInput.value = "";
 
+    //auto scroll to bottom
+    document
+        .getElementById("messages")
+        .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+
+    // create db collection and send in the data
+    db.ref("messages/" + timestamp).set({
+        username,
+        message,
     });
-    commentfiled.innerHTML = out;
 }
 
-function timeConverter(UNIX_timestamp) {
-    let a = new Date(UNIX_timestamp);
+// display the messages
+// reference the collection created earlier
+const fetchChat = db.ref("messages/");
 
-    let time = a.toString();
-    return time;
-}
+// check for new messages using the onChildAdded event listener
+fetchChat.on("child_added", function(snapshot) {
+    const messages = snapshot.val();
+    const message = `<li class=${
+      username === messages.username ? "sent" : "receive"
+    }><span>${messages.username}: </span>${messages.message}</li>`;
+    // append the message on the page
+    document.getElementById("messages").innerHTML += message;
+});
